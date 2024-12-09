0752ed51b638a9ff3aa86b846b887203b887443d
Phurinho <phurintoomkul@gmail.com>
2024-12-09 20:57:39 +0700

    'coffee'

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ ScrollReveal().reveal('.home-contact p, .about-content',{origin:'right'});
 
  /*====================== typed js ================= */
  const typed = new Typed('.multiple-text',{
-    strings:['Frontend Developer','Web Designer','Tiktoker'],
+    strings:['Frontend Developer','Backend Developer','Full-Stack Developer'],
     typeSpeed:70,
     backSpeed:70,
     backDelay:1000,
